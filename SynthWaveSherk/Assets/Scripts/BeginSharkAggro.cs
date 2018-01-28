a24f316606d6b9fdac02ad3083e66a53618ab12d
jawebste <32810524+jawebste@users.noreply.github.com>
2018-01-28 14:35:43 -0800

    EVEN MORE SOUNDS

--- SynthWaveSherk/Assets/Scripts/BeginSharkAggro.cs
+++ SynthWaveSherk/Assets/Scripts/BeginSharkAggro.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BeginSharkAggro : MonoBehaviour {
+    public AudioClip landSound;
+
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Shark")
         {
+            source.PlayOneShot(landSound);
             Debug.Log("Begin Attack!");
             other.gameObject.GetComponent<MoveSharkScript>().StartAttack();
         }
