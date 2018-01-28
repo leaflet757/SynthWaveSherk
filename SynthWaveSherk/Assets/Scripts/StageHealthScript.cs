89ea92173f259beaf29cbf9dae68ddee01ff6d9d
leaflet757 <leifmyer@gmail.com>
2018-01-28 13:57:26 -0800

    added combined health/timer script

--- SynthWaveSherk/Assets/Scripts/StageHealthScript.cs
+++ SynthWaveSherk/Assets/Scripts/StageHealthScript.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class StageHealthScript : MonoBehaviour {
     float remainingHP = 30F;
 
+    public event Timer.OnStageHit hitStage;
+
+
     private void OnCollisionEnter(Collision collision)
     {
+        hitStage.Invoke(gameObject);
+
         remainingHP -= 5F;
 
         if(remainingHP <= 0)
         {
-            print("deed");
+            // Do stages need their own sepporate health if they are tied to the timer?
+            print("Stage is Dead " + name);
         }
     }
 }
--- SynthWaveSherk/Assets/Scripts/Timer.cs
+++ SynthWaveSherk/Assets/Scripts/Timer.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Timer : MonoBehaviour {
-    float timeLeft = 30F;       // time before ship blows: 30s
-	
-	// Update is called once per frame
-	void Update () {
-       // timeLeft -= Time.deltaTime;
-		if(timeLeft < 0)
+    public float timeLeft = 30F;       // time before ship blows: 30s
+                                       // gonna treat 'timeLeft' as a combined health total 
+                                       // because it's quick to implement
+
+    public GameObject Stage1;
+    public GameObject Stage2;
+
+    public delegate void OnStageHit(GameObject stage);
+
+
+    private void Start()
+    {
+        Stage1.GetComponent<StageHealthScript>().hitStage += Timer_hitStage;
+        Stage2.GetComponent<StageHealthScript>().hitStage += Timer_hitStage;
+    }
+
+    private void Timer_hitStage(GameObject damagedStage)
+    {
+        timeLeft -= 1;
+        if (timeLeft < 0)
         {
-            Time.timeScale = 0;     // just pauses time, need to end game
+            //Time.timeScale = 0;     // just pauses time, need to end game
+            Debug.Log("The Game is over!");
+
+            // TODO: play explosion here and game over
         }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        //timeLeft -= Time.deltaTime;
+	    
 	}
 }
