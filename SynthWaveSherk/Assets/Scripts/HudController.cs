47df272575c869fdca1e94bebbc2ddcbf5da165b
leaflet757 <leifmyer@gmail.com>
2018-01-27 17:38:28 -0800

    added shark tag and crosshair update

---
+++ SynthWaveSherk/Assets/Scripts/HudController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudController : MonoBehaviour {
+
+    public GameObject PlayerCamera = null;
+
+    public Image CrosshairNone = null;
+    public Image CrosshairLock = null;
+
+    public string SharkTag = "Shark";
+
+    private void Start()
+    {
+        CrosshairLock.enabled = false;
+    }
+
+    void LateUpdate ()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.forward, out hitInfo))
+        {
+            if (hitInfo.collider.tag == SharkTag)
+            {
+                CrosshairLock.enabled = true;
+            }
+        }
+        else
+        {
+            CrosshairLock.enabled = false;
+        }
+	}
+}
