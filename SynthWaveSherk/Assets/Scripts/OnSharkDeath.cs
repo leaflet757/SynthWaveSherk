e625c6bd1cc5ae6bde6ea42e5577594a061b0e97
jawebste <32810524+jawebste@users.noreply.github.com>
2018-01-28 13:55:36 -0800

    shark death sound

--- SynthWaveSherk/Assets/Scripts/OnSharkDeath.cs
+++ SynthWaveSherk/Assets/Scripts/OnSharkDeath.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class OnSharkDeath : OnDeathScript
 {
-    public AudioClip deathSound1;
     public AudioClip deathSound2;
 
     private AudioSource source;
 
-    void Awake()
+    void Start()
     {
         source = GetComponent<AudioSource>();
     }
-
+    
     public override void PerformDeathActionAndDestroy()
     {
-        source.PlayOneShot(deathSound2, 1f);
         Debug.Log("TODO: OnSharkDeath");
-        Destroy(gameObject);
+        source.PlayOneShot(deathSound2);
+        
+        Destroy(gameObject,2.32f);
     }
 }
